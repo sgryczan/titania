651b701f36019af3e91907d79ce1cb7ab6403153
Sebastian Gryczan <bash@czan.io>
2020-08-08 19:23:29 -0600

    update folder path

--- k8s/tf-build/kubespray/main.tf
+++ k8s/tf-build/kubespray/main.tf
@@ -27,7 +27,7 @@ module "infra" {
 
     // /${DATACENTER}/host/${CLUSTER}/Resources/${RESOURCE_POOL}
     resource_pool_name          = var.resource_pool_name
-    folder                      = "${var.folder}/${var.vm_name_prefix}"
+    folder                      = "${var.folder}"
     datacenter_name             = var.datacenter_name
     vm_template_name            = var.vm_template_name
 }
