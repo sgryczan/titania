a3f6c3683ed99d56e15a26a9cba6d308722b760f
Sebastian Gryczan <bash@czan.io>
2020-08-08 22:07:37 -0600

    inject provider

--- k8s/tf-build/kubespray/main.tf
+++ k8s/tf-build/kubespray/main.tf
@@ -7,9 +7,22 @@ provider "aws" {
   alias = "k8s"
 }
 
+provider "vsphere" {
+  user           = "${var.vsphere_user}"
+  password       = "${var.vsphere_password}"
+  vsphere_server = "${var.vsphere_server}"
+  alias = "k8s"
+  
+  # If using a self-signed cert
+  allow_unverified_ssl = true
+}
+
 module "infra" {
     source =  "../modules/k8s-vm"
 
+    providers = {
+      vsphere = "vsphere.k8s"
+    }
     vsphere_user                = "${var.vsphere_user}"
     vsphere_password            = "${var.vsphere_password}"
     vsphere_server              = "${var.vsphere_server}"
