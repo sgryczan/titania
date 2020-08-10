dec7bb2b1dce6f6a3e0f1bcdd1c6fdb40c2410ab
Sebastian Gryczan <bash@czan.io>
2020-08-09 22:32:00 -0600

    updates

--- k8s/tf-build/modules/kubernetes/trident/hack/script.sh
+++ k8s/tf-build/modules/kubernetes/trident/hack/script.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-# ssh_key_generator - designed to work with the Terraform External Data Source provider
-#   https://www.terraform.io/docs/providers/external/data_source.html
-#  by Irving Popovetsky <irving@popovetsky.com> 
-#
-#  this script takes the 3 customer_* arguments as JSON formatted stdin
-#  produces public_key & private_key (contents) and the private_key_file (path) as JSON formatted stdout
-#  DEBUG statements may be safely uncommented as they output to stderr
 
 function error_exit() {
   echo "$1" 1>&2
@@ -13,17 +6,17 @@ function error_exit() {
 }
 
 function check_deps() {
-   test -f $(which tridentctl) || error_exit "tridentctl command not detected in path, please install it"
+  test -f $(which tridentctl) || error_exit "tridentctl command not detected in path, please install it"
   test -f $(which kubectl) || error_exit "kubectl command not detected in path, please install it"
   test -f $(which jq) || error_exit "jq command not detected in path, please install it"
 }
 
 function parse_input() {
-  # jq reads from stdin so we don't have to set up any inputs, but let's validate the outputs
-  eval "$(jq -r '@sh "export NAMESPACE=\(.namespace) BACKEND=\(.backend) STORAGECLASSES=\(.storageclasses)"')"
-  if [[ -z "${NAMESPACE}" ]]; then export NAMESPACE=none; fi
-  if [[ -z "${BACKEND}" ]]; then export BACKEND=none; fi
-  if [[ -z "${STORAGECLASSES}" ]]; then export STORAGECLASSES=none; fi
+  input=$(jq -r '.')
+  echo "DEBUG: input: ${input}" 1>&2
+  for query_value in $(echo "${input}" | jq -r 'to_entries|map("\(.key)=\(.value|tostring)")|.[]'); do
+    export ${query_value}
+  done
 }
 
 function install_trident() {
@@ -48,7 +41,6 @@ function produce_output() {
 
 # main()
 check_deps
-# echo "DEBUG: received: $INPUT" 1>&2
 parse_input
 install_trident
 create_backend
